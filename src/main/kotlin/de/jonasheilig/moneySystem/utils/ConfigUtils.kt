9c056cf72ee0e21c52c81709be5a24b1bd9bf408
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-08-03 21:08:19 +0200

    add

--- src/main/kotlin/de/jonasheilig/moneySystem/utils/ConfigUtils.kt
+++ src/main/kotlin/de/jonasheilig/moneySystem/utils/ConfigUtils.kt
@@ -1,4 +1,40 @@
 package de.jonasheilig.moneySystem.utils
 
-class ConfigUtils {
+import de.jonasheilig.moneySystem.MoneySystem
+import org.bukkit.configuration.file.YamlConfiguration
+import java.io.File
+import java.util.UUID
+
+object ConfigUtils {
+    private lateinit var moneyFile: File
+    private lateinit var moneyConfig: YamlConfiguration
+
+    fun setupMoneyFile(plugin: MoneySystem) {
+        moneyFile = File(plugin.dataFolder, "money.yml")
+        if (!moneyFile.exists()) {
+            moneyFile.createNewFile()
+        }
+        moneyConfig = YamlConfiguration.loadConfiguration(moneyFile)
+    }
+
+    fun getMoney(uuid: UUID): Int {
+        return moneyConfig.getInt(uuid.toString(), getDefaultMoney())
+    }
+
+    fun setMoney(uuid: UUID, amount: Int) {
+        moneyConfig.set(uuid.toString(), amount)
+        saveMoneyFile()
+    }
+
+    fun hasMoney(uuid: UUID): Boolean {
+        return moneyConfig.contains(uuid.toString())
+    }
+
+    fun getDefaultMoney(): Int {
+        return MoneySystem.getPlugin(MoneySystem::class.java).config.getInt("default-money", 100)
+    }
+
+    private fun saveMoneyFile() {
+        moneyConfig.save(moneyFile)
+    }
 }
